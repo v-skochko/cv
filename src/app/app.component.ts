0f3d2ee05736b4735dd2e4a08b9b99898ba98f9a
Skochko <Skochkovs@gmail.com>
2019-02-25 09:33:06 +0200

    fix linkedin url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,12 @@ import {slider} from './route-animations';
 
 @Component({
   selector: 'app-root',
-
   animations: [
-
     slider
-
   ],
-
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-
 export class AppComponent {
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
